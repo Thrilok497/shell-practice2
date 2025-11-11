083071e5b406cdbb371b28460bbbe8f3d52bd1d2
Thrilok497 <devarpallithrilok@gmail.com>
2025-11-11 11:27:41 +0530

    first

--- hello-world.sh
+++ hello-world.sh
@@ -1,9 +1,15 @@
 #!/bash/bin
 
-PERSON1=$1
-PERSON2=$2
-echo "$PERSON1::hiii,$PERSON2"
-echo "$PERSON2::hiii,$PERSON1"
-echo "current directory::$PWD"
-echo "RAM Memory::$free"
+# PERSON1=$1
+# PERSON2=$2
+# echo "$PERSON1::hiii,$PERSON2"
+# echo "$PERSON2::hiii,$PERSON1"
+# echo "current directory::$PWD"
 
+NUMBER=$1
+
+if [$NUMBER is -lt 10];then
+    echo "given $NUMBER is less than 10"
+else
+    echo "given $NUMBER is not less than 10"
+fi
